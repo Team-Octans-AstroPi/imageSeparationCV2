a4825fb56b38e2b622d118b6d46286005467e084
iCMDgithub <80255379+iCMDgithub@users.noreply.github.com>
2023-01-22 13:28:36 +0200

    Added dominant plant health computation using KMeans clustering

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sys
 import numpy as np
 import cv2
 from fastiecm import fastiecm
+from sklearn.cluster import MiniBatchKMeans as KMeans # Either MiniBatchKMeans (faster) or KMeans (more accurate)
+import time
 import os
-
 
 def contrast_stretch(im):
     in_min = 0.0
@@ -57,16 +58,54 @@ waterDown = np.array([0, 0, 30])
 
 water = cv2.bitwise_or(water, cv2.bitwise_and(img, img, mask = cv2.inRange(img, waterDown, waterUp)))
 
-cv2.imshow('Initial Image', cv2.cvtColor(img, cv2.COLOR_HSV2BGR))
-cv2.imshow('Clouds', cv2.cvtColor(clouds, cv2.COLOR_HSV2BGR))
-cv2.imshow('Water', cv2.cvtColor(water, cv2.COLOR_HSV2BGR))
-cv2.imshow('NDVI Raw', calc_ndvi(cv2.cvtColor(img, cv2.COLOR_HSV2BGR)))
+#cv2.imshow('Initial Image', cv2.cvtColor(img, cv2.COLOR_HSV2BGR))
+#cv2.imshow('Clouds', cv2.cvtColor(clouds, cv2.COLOR_HSV2BGR))
+#cv2.imshow('Water', cv2.cvtColor(water, cv2.COLOR_HSV2BGR))
+#cv2.imshow('NDVI Raw', calc_ndvi(cv2.cvtColor(img, cv2.COLOR_HSV2BGR)))
 
 land = cv2.subtract(img, cv2.add(cv2.add(clouds, water), window))
-cv2.imshow('Land', cv2.cvtColor(land, cv2.COLOR_HSV2BGR))
+#cv2.imshow('Land', cv2.cvtColor(land, cv2.COLOR_HSV2BGR))
 toBeColorMapped = contrast_stretch(calc_ndvi(cv2.cvtColor(land, cv2.COLOR_HSV2BGR))).astype(np.uint8)
-cv2.imshow('NDVI', toBeColorMapped)
+#cv2.imshow('NDVI', toBeColorMapped)
+
+ndvicm = cv2.applyColorMap(toBeColorMapped, fastiecm)
 cv2.imshow('Color Mapped NDVI', cv2.applyColorMap(toBeColorMapped, fastiecm))
+
+"""
+    Dominant Plant Health
+    This uses KMeans to compute the most 
+"""
+
+# scale down the original image to make KMeans faster
+scale = 0.25 
+w = int(toBeColorMapped.shape[1] * scale)
+h = int(toBeColorMapped.shape[0] * scale)
+print("Kmeans rescaled image size:", (w, h))
+toBeColorMapped = cv2.resize(toBeColorMapped, (w, h), interpolation = cv2.INTER_LINEAR)
+
+# Scikit-learn KMeans Cluster implementation
+pixels = np.uint8(toBeColorMapped.reshape((-1, 1))) # convert image to 1D array of elements (element = array of one value)
+print("Finding KMeans clusters...\nFound clusters in", end=" ")
+ms = time.time()*1000.0
+kmeans = KMeans(4, max_iter=1024)
+kmeans.fit(pixels)
+print(time.time()*1000.0-ms, "ms") # print time taken to compute KMeans, useful when searching for the best max_iter, scale, and KMeans type.
+
+# print data
+print("Average plant health: ", np.average(ndvicm))
+print("Median plant health: ", np.median(ndvicm))
+print("85th Percentile plant health: ", np.percentile(toBeColorMapped, 85))
+print("K-Means Clustering:\n", kmeans.cluster_centers_)
+
+# show computed dominant colors in OpenCV windows
+for color in range(0, np.shape(kmeans.cluster_centers_)[0]):
+    array = [[kmeans.cluster_centers_[color][0] for _ in range(200)] for _ in range(200)]
+    array = np.array(array).astype(np.uint8)
+
+    cv2.cvtColor(array, cv2.COLOR_GRAY2BGR)
+
+    cv2.imshow("Dominant plant health "+str(color), cv2.applyColorMap(array, fastiecm))
+
 
 # wait until q is pressed on the CV2 window
 while True:
